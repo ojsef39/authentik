77540854717bd26defa46030821fd0d3c5623d96
Teffen Ellis <teffen@goauthentik.io>
2025-06-17 23:16:54 +0200

    WIP: Disable API build temp.

--- website/sidebars/docs.mjs
+++ website/sidebars/docs.mjs
@@ -3,7 +3,7 @@
  *
  * @import { SidebarItemConfig } from "@docusaurus/plugin-content-docs-types"
  */
-import apiReference from "../docs/developer-docs/api/reference/sidebar";
+// import apiReference from "../docs/developer-docs/api/reference/sidebar";
 import { generateVersionDropdown } from "../src/utils.js";
 
 /**
@@ -643,11 +643,11 @@ const items = [
                     "developer-docs/api/flow-executor",
                     "developer-docs/api/making-schema-changes",
                     "developer-docs/api/websocket",
-                    {
-                        type: "category",
-                        label: "Reference",
-                        items: apiReference,
-                    },
+                    // {
+                    //     type: "category",
+                    //     label: "Reference",
+                    //     items: apiReference,
+                    // },
                     "developer-docs/api/clients",
                 ],
             },
